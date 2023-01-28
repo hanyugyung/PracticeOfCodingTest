3d7dd7f8165d65b4ab6a463e7baf1cf30ebe70a9
han60 <dbruddl20202@naver.com>
2023-01-28 11:19:28 +0900

    - 프로그래머스 삼총사(wip)

---
+++ src/main/kotlin/Quiz_32.kt
@@ -0,0 +1,23 @@
+fun solution32(number: IntArray): Int {
+    var answer: Int = 0
+
+    for(i in 0..number.size - 1) {
+        for(j in 0.. number.size - 1) {
+            for(k in 0..number.size - 1) {
+                if(i != j && j != k) {
+                    if(number[i] + number[j] + number[k] == 0) answer++
+                }
+            }
+        }
+    }
+
+    return answer / 6 // 3개를 뽑아 특정 수 0을 만드는 경우 순서를 고려하지 않으므로 3! 만큼 나눈다
+}
+
+fun main(args: Array<String>) {
+    println(
+        solution32(
+            intArrayOf(0, 0, 0)
+        )
+    )
+}
